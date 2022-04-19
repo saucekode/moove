732437ee96cbdf6a0f15938830aaa4d8019ff519
saucekode <saucekoder@gmail.com>
2022-04-18 19:54:16 -0700

    added exception handler

--- user/src/main/java/com/shoplyst/user/web/exceptions/AppException.java
+++ user/src/main/java/com/shoplyst/user/web/exceptions/AppException.java
@@ -1,5 +1,9 @@
 package com.shoplyst.user.web.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class AppException extends RuntimeException{
     public AppException(String message) {
         super(message);
--- user/src/main/java/com/shoplyst/user/web/exceptions/ResourceNotFoundException.java
+++ user/src/main/java/com/shoplyst/user/web/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,11 @@
 package com.shoplyst.user.web.exceptions;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Data
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     private String resourceName;
     private String fieldName;
