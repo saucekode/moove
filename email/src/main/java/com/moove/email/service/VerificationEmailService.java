b328b0dc2dc82cd2458691bc790b13d7d047220f
saucekode <saucekoder@gmail.com>
2022-06-29 10:46:09 -0700

    added verification email method

---
+++ email/src/main/java/com/moove/email/service/VerificationEmailService.java
@@ -0,0 +1,10 @@
+package com.moove.email.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VerificationEmailService {
+    public void sendVerificationEmail(String siteURL){
+
+    }
+}
